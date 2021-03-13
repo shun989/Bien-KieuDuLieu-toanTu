5dba27dff82a3d0e9971e0ebde2ab1cf23ba66f8
Your Name <you@example.com>
2021-03-13 14:40:07 +0700

    file js của SD toán tử

---
+++ SDToanTuu.js
@@ -0,0 +1,10 @@
+let inputWidth= prompt("Enter the width");
+let inputHeight= prompt("Enter the height");
+let width= parseInt(inputWidth);
+   document.write('Enter width: ' + width);
+   document.write("<br/>");
+let height= parseInt(inputHeight);
+   document.write('Enter height: ' + height);
+   document.write("<br/>");
+let area= width * height;
+   document.write("The area is: " + area);
